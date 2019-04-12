fb719f653b4fa53bb7feb5281e9b7dcaacb88c6e
Le_Futuriste <spamfree@matthieubessat.fr>
2019-04-12 18:53:28 +0200

    refactor: add overlay class

--- app.js
+++ app.js
@@ -3,34 +3,7 @@ let consoleId = process.env.CONSOLE_ID !== undefined ? process.env.CONSOLE_ID : 
 
 console.log("I'm " + consoleId)
 
-const io = require('socket.io-client')
-const wifi = require('node-wifi')
-wifi.init({
-    iface: null
-});
-const socket = io(socketUrl, {
-    transportOptions: {
-        polling: {
-            extraHeaders: {
-                'x-client-type': 'console',
-                'x-console-id': consoleId
-            }
-        }
-    }
-});
+const Overlay = require('./src/overlay')
+const overlay = new Overlay(socketUrl, consoleId)
 
-
-
-socket.on('connect', function () {
-    socket.on('get-status', function (callback) {
-        callback({temp: 56.8})
-    });
-    socket.on('get-wifi', function (callback) {
-        wifi.getCurrentConnections((err, currentConnections) => {
-            if (err) {
-                console.log(err);
-            }
-            callback(currentConnections[0].ssid)
-        });
-    })
-});
+overlay.init()
---
+++ src/overlay.js
@@ -0,0 +1,54 @@
+const childProcess = require('child_process')
+const wifi = require('node-wifi')
+const io = require('socket.io-client')
+
+module.exports = class Overlay {
+    constructor(socketUrl, consoleId) {
+        this.socket = io(socketUrl, {
+            transportOptions: {
+                polling: {
+                    extraHeaders: {
+                        'x-client-type': 'console',
+                        'x-console-id': consoleId
+                    }
+                }
+            }
+        });
+    }
+    init () {
+        wifi.init({
+            iface: null
+        });
+        this.socket.on('connect', function () {
+            this.socket.on('get-status', getStatus);
+            this.socket.on('get-wifi', getWifi)
+        });
+    }
+    getStatus (callback) {
+        status = {isAlive: true}
+        this.getTemp().then(temp => {
+            status.temp = temp
+            return callback(status)
+        })
+    }
+    getWifi (callback) {    
+        wifi.getCurrentConnections((err, currentConnections) => {
+            if (err) {
+                console.log(err)
+                return;
+            } else {
+                callback(currentConnections[0])
+            }
+        })        
+    }
+    getTemp () {
+        return new Promise((resolve, reject) => {
+            childProcess.exec('vcgencmd measure_temp', (err, stdout, stderr) => {
+                if (err && stderr != '') {
+                    return reject()
+                }
+                return resolve(parseFloat(stdout.replace('temp=', '').replace("'C")))
+            })
+        })
+    }
+}
