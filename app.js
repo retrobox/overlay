d3ee4198cf2288e929b7a209b60264adf6bd7329
lefuturiste <spamfree@matthieubessat.fr>
2019-11-01 22:44:25 +0000

    feat: add dotenv file support

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const dotenv = require('dotenv')
+dotenv.config()
+
 let socketUrl = process.env.SOCKET_URL !== undefined ? process.env.SOCKET_URL : 'https://ws.retrobox.tech'
 let consoleId = process.env.CONSOLE_ID !== undefined ? process.env.CONSOLE_ID : null
 let consoleToken = process.env.CONSOLE_TOKEN !== undefined ? process.env.CONSOLE_TOKEN : null
