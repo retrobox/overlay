24c43bc8fc39999d2e06d58ea626e3a120c5b18d
Matthieu Bessat <spamfree@matthieubessat.fr>
2020-04-04 17:46:59 +0100

    feat: add self kill event handling

--- src/Overlay.js
+++ src/Overlay.js
@@ -40,6 +40,7 @@ module.exports = class Overlay {
             this.socket.off('terminal-resize')
             this.socket.off('close-terminal-session')
             this.socket.off('ping-check')
+            this.socket.off('self-kill')
 
             this.socket.on('get-status', (callback) => this.getStatus(callback))
             this.socket.on('shutdown', (callback) => this.shutdown(callback))
@@ -49,6 +50,7 @@ module.exports = class Overlay {
             this.socket.on('terminal-resize', (data) => this.terminalResize(data))
             this.socket.on('close-terminal-session', () => this.closeTerminalSession())
             this.socket.on('ping-check', (callback) => this.respondPing(callback))
+            this.socket.on('self-kill', (callback) => this.selfKill(callback))
             console.log('> Connected with websocket server')
         });
         this.socket.on('disconnect', () => {
@@ -230,6 +232,14 @@ module.exports = class Overlay {
         callback({ack: true})
         setTimeout(() => {
             childProcess.execSync('reboot')
+        }, 200)
+    }
+
+    async selfKill(callback) {
+        callback({ack: true})
+        console.log('> Received the order of auto killing my self :(')
+        setTimeout(() => {
+            process.exit()
         }, 200)
     }
 }
